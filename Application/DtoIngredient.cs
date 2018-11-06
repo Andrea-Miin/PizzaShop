5c9ae20aa49b9c8e633726401073bc4767a4c31c
JoseYepes <Yepes_jose@hotmail.com>
2018-11-06 14:31:22 +0100

    Icontext ingredient

---
+++ Application/DtoIngredient.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application
+{
+    public class DtoIngredient
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public Pizza Pizza { get; set; }
+        static void create() { }
+    }
+    /*public abstract class DtoIngredient : Ingredient
+   {
+       public DtoIngredient = Ingredient ingredient
+       {
+           this.Name = ingredient.Name
+           this.Price = ingredient.Price
+           this.Pizza = ingredient.Pizza
+           this.Create = ingredient.Create
+       }
+
+   }*/
+}
+
