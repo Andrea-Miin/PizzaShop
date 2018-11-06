3f0dbf1b6506112f86d2f79bccfa12266557e070
saekkimiin <saekkimiin@gmail.com>
2018-11-06 10:14:35 +0100

    Agregado Pizza

--- PizzaShop/Domain/Pizza.cs
+++ PizzaShop/Domain/Pizza.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace Domain
 {
-    public class Pizza
+    public class Pizza : EntityBase
     {
+        public string Name { get; set; }
+        public virtual ICollection<Ingredient> Id_Ingredient { get; set; }
+        public virtual ICollection<Comment> Id_Comment { get; set; }
+        public byte[] Photo { get; set; }
+        public decimal Price { get; set; }
     }
 }
